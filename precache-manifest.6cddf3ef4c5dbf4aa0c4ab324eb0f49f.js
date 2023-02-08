d95f0f436a1e85abce3073864399d10025018695
Serkan Korkut <serkan.korkut@thundra.io>
2023-02-08 20:27:47 +0300

    Updates

---
+++ precache-manifest.6cddf3ef4c5dbf4aa0c4ab324eb0f49f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9a65f6e20522bfa9d895e3a642278e",
+    "url": "/afet-yardim-fe-pub/index.html"
+  },
+  {
+    "revision": "8dff5add7a5a2a115998",
+    "url": "/afet-yardim-fe-pub/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "d1d07fda1850e89c0dcf",
+    "url": "/afet-yardim-fe-pub/static/css/main.22d2b07a.chunk.css"
+  },
+  {
+    "revision": "8dff5add7a5a2a115998",
+    "url": "/afet-yardim-fe-pub/static/js/2.da5947b1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/afet-yardim-fe-pub/static/js/2.da5947b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d07fda1850e89c0dcf",
+    "url": "/afet-yardim-fe-pub/static/js/main.4d3dabe6.chunk.js"
+  },
+  {
+    "revision": "32143d6ed8add43a9169",
+    "url": "/afet-yardim-fe-pub/static/js/runtime-main.d921b1d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/afet-yardim-fe-pub/static/media/logo.5d5d9eef.svg"
+  }
+]);
